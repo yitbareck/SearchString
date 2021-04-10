ed357a07920e4e8c76b1e6c0724c1f29b3d7034f
Yitbarek Z <ytbareck@gmail.com>
2021-04-10 16:35:29 +0300

    Initial Commit

--- src/com/manyahl/libs/Demo.java
+++ src/com/manyahl/libs/Demo.java
@@ -5,8 +5,8 @@ import java.math.BigInteger;
 public class Demo {
     public static void main(String[] args) {
         try{
-            System.out.println(StringSearch.findString(new BigInteger("32268744561300"), StringSearch.CharSet.LOWER_UPPER));
-            System.out.println(StringSearch.findIndex("Ethiopia", StringSearch.CharSet.LOWER_UPPER));
+            System.out.println(StringSearch.findString(new BigInteger("73727"), StringSearch.CharSet.LOWER));
+            System.out.println(StringSearch.findIndex("dear", StringSearch.CharSet.LOWER));
         }catch(InvalidIndexException | StringAndCharsetMismatchException e){
             System.out.println(e.getMessage());
         }
